b583e0e237816eaf05294ca9c3b5eaaecbfdd3c0
Luiz Chequini <luizchequini@gmail.com>
2021-03-20 16:41:15 -0300

    Introdução e setup da API - Introdução e biblioteca Swasbuckle.AspNetCore.Annotations

--- Curso/Curso.Api/Controllers/UsuarioController.cs
+++ Curso/Curso.Api/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
-﻿using Curso.Api.Models.Usuarios;
+﻿using Curso.Api.Models;
+using Curso.Api.Models.Usuarios;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Curso.Api.Controllers
 {
@@ -7,6 +9,14 @@ namespace Curso.Api.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        /// <summary>
+        /// teste
+        /// </summary>
+        /// <param name="loginViewModelInput"></param>
+        /// <returns></returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
         [HttpPost]
         [Route("logar")]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
