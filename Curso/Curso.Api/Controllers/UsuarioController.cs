f99fd873a864fe84410da1be5e0c039dd71d8912
Luiz Chequini <luizchequini@gmail.com>
2021-03-20 15:11:08 -0300

    Introdução e setup da API - Conheça o Postman

---
+++ Curso/Curso.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using Curso.Api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
